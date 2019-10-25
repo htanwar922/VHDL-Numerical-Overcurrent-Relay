53051d4ff87f8150dc26a2bc0e7c643d90ae64ff
Himanshu <htanwar922@gmail.com>
2019-10-25 19:09:36 +0530

    update

--- t_check.vhd
+++ t_check.vhd
@@ -14,25 +14,34 @@ end testbench;
 
 architecture behavior of testbench is
 	component check
-		port( a : in ufixed(4 downto -4) := "001100011";
+		port( a : in ufixed(4 downto -4);
 				y : out float(5 downto -3)
 			);
 	end component;
 
 	signal t_a : ufixed(4 downto -4) := "001100011";
 	signal t_y : float(5 downto -3);
+	signal clk : bit;
 
 begin
-	uut: check port map(a => t_a, y => t_y);
-
+	t_check: check port map(a => t_a, y => t_y);
+	
+	clock : process
+	begin
+		clk <= '0';
+		wait for 10 ns;
+		clk <= '1';
+		wait for 10 ns;
+	end process;
+	
 	tb : process
 	begin
-
+		--t_a := "001100011";
 		wait for 1000 ns; -- wait until global set/reset completes
 
 		t_a <= "101101101";
 
-		wait; -- will wait forever
+		wait for 1000 us; -- will wait forever
 	end process tb;
 	
 end;
