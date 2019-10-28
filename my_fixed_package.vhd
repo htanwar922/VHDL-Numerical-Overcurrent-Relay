747579ac302762b1c0d4890efc89f2a0e6e13696
Himanshu <htanwar922@gmail.com>
2019-10-28 21:53:47 +0530

    Update my_fixed_package.vhd

--- my_fixed_package.vhd
+++ my_fixed_package.vhd
@@ -23,8 +23,12 @@ package my_fixed_package is
 
 subtype sfixed_2c is fixed;
 
+--function sfixed2ufixed(arg : sfixed; ch : choice := 0) return ufixed;	--choice 0: change sign bit to 0, choice 1: truncate sign bit choice 2: return as it is
+--function ufixed2sfixed(arg : ufixed) return sfixed;																																								--checked
+
 function sm2twoc(arg : sfixed) return sfixed_2c;		--sign-magnitude to two's complement
 function twoc2sm(arg : sfixed_2c) return sfixed;		--increases vector length by 1 for sign-bit
+--function sfixed2signed(arg : sfixed) return ieee.numeric_std.signed;
 
 function "+"(l,r : sfixed) return sfixed;
 --function "-"(l,r : sfixed) return sfixed;
@@ -32,11 +36,40 @@ function "+"(l,r : sfixed) return sfixed;
 --function "/"(l,r : sfixed) return sfixed;
 --function "abs"(arg : sfixed) return sfixed;	--append '0' to left for sign bit = '1' and leftmost = integer'low
 
+--function join_fixed(l,r : ufixed) return ufixed;																--decimal point of r preserved
+
+
 end my_fixed_package;
 
 --============================================================================================================================================================================================
 
 package body my_fixed_package is
+
+function join_fixed(l,r : fixed) return fixed is
+	variable high : integer := l'length + r'high;
+	variable low : integer := r'low;
+	variable result : fixed(high downto low);
+begin
+	result(high downto r'high+1) := l;
+	result(r'high downto low) := r;
+	return result;
+end function join_fixed;
+
+function sfixed2ufixed(arg : sfixed; ch : choice := 0) return ufixed is
+	variable result : ufixed(arg'high-1 downto arg'low) := arg(arg'high-1 downto arg'low);
+begin
+	if(ch=0) then return fixed('0' & to_slv(arg));														--choice 0: append 0			--join_sl_fixed('0',result);
+	else return result;																							--choice 1: truncate s-bit
+	end if;
+end function sfixed2ufixed;
+
+function ufixed2sfixed(arg : ufixed) return sfixed is
+	variable result : sfixed(arg'high+1 downto arg'low);
+begin
+	result := fixed('0' & to_slv(arg)); 																	--join_sl_fixed('0',arg);
+	return result;
+end function ufixed2sfixed;
+
 
 function sm2twoc(arg : sfixed) return sfixed_2c is
 	variable result : sfixed(arg'high downto arg'low) := arg;
