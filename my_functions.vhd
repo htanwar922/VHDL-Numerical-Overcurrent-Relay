23f499e092dbaf535cfde5cc7767e708c0367587
Himanshu <htanwar922@gmail.com>
2019-11-06 03:25:07 +0530

    print procedure writeproc updated

--- my_functions.vhd
+++ my_functions.vhd
@@ -9,6 +9,7 @@ package my_functions is
 
 function minimum(l,r : integer) return integer;
 function maximum(l,r : integer) return integer;
+procedure writeproc(sig: in std_logic_vector; s : string);
 
 end my_functions;
 
@@ -28,12 +29,21 @@ begin
    end if;
 end function maximum;
 
-procedure writeproc ( sig: in std_logic_vector) is
-  variable li : line;
-  file output : text open write_mode is "output";
-  begin
-    write(li, std_logic_vector(sig));
-    writeline(output, li);
+procedure writeproc(sig: in std_logic_vector; s : string) is
+	variable li : line;
+	variable str : string(1 to sig'length);
+	file f_in : text; -- open read_mode is "output";
+	file f_out : text; -- open write_mode is "output";
+begin
+	file_open(f_out, "output", write_mode);
+	write(li, std_logic_vector(sig));
+   write(li, lf);	writeline(f_out, li);
+	file_close(f_out);
+	file_open(f_in, "output", read_mode);
+	readline(f_in, li);
+	read(li, str);
+	file_close(f_in);
+	report s & str;
 end procedure writeproc;
  
 end my_functions;
