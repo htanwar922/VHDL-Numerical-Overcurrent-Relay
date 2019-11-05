54f05f481b66909cea9fc2eab1045998c70d7c98
Himanshu <htanwar922@gmail.com>
2019-11-06 03:21:48 +0530

    Update t_check.vhd

--- t_check.vhd
+++ t_check.vhd
@@ -15,21 +15,24 @@ end testbench;
 
 architecture behavior of testbench is
 	component check
-		port( a : in float(6 downto -7);
-				y : out float(6 downto -7);
-				y2 : out sfixed(7 downto -7)
+		port( a : in float(1 downto -7);
+--				y : out float(6 downto -7);
+--				y2 : out sfixed(7 downto -7)
+				sqr : out float(0 downto -14)
 			);
 	end component;
 
-	signal t_a : float(6 downto -7) := "11000111100110"; --:= "11";
-	signal t_y : float(6 downto -7);
-	signal t_y2 : sfixed(7 downto -7);
+	signal t_a : float(1 downto -7) := "110000000"; --"11000111100110"; --:= "11";
+--	signal t_y : float(6 downto -7);
+--	signal t_y2 : sfixed(7 downto -7);
+	signal sqr : float(0 downto -14);
 	signal clk : bit;
 
 begin
 	t_check: check port map(a => t_a,
-									y => t_y,
-									y2 => t_y2
+--									y => t_y,
+--									y2 => t_y2
+									sqr => sqr
 									);
 	
 	clock : process
@@ -42,13 +45,13 @@ begin
 	
 	tb : process
 	begin
-		t_a <= "01001111100110";
+--		t_a <= "100000000"; --"01001111100110";
 		wait for 300 ns; -- wait until global set/reset completes
 
-		t_a <= "00111010100110";
+		t_a <= "010011000"; --"00111010100110";
 		wait for 300 ns;
 		
-		t_a <= "00111001100110";
+		t_a <= "010000001"; --"00111001100110";
 
 		wait for 1000 ns;
 	end process tb;
