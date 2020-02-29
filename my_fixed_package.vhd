7fb02e3a2e0aae724c7cb588bce7a8b044a2fefe
Himanshu <htanwar922@gmail.com>
2020-03-01 03:07:57 +0530

    Simplest fixed pkg - use this for now

---
+++ my_fixed_package.vhd
@@ -0,0 +1,23 @@
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+use std.textio.all;
+use ieee.std_logic_textio.all;
+
+use work.my_functions.all;
+use work.my_types.all;
+
+package my_fixed_package is
+
+
+
+end my_fixed_package;
+
+
+package body my_fixed_package is
+
+
+
+end my_fixed_package;
